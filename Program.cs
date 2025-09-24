030c9e91904a6c1fba780449a7a00533d7e88434
WajihaAkhtar <wajihaakhtar30@gmail.com>
2025-09-24 10:04:42 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+class Program
+{
+    static void Main()
+    {
+        string m = ".......";
+        int length = m.Length;
+        for (int i = 0; i < length; i++)
+        {
+            Console.Write("\r" + new string(' ', i) + "C" + m.Substring(i + 1));
+            Thread.Sleep(200);
+            Console.Write("\r" + new string(' ', i + 1) + m.Substring(i + 1));
+            Thread.Sleep(200);
+        }
+        Console.Write("\r" + new string(' ', length) + "C");
+        Console.WriteLine("\n\nGame over! Pac-Man ate all the dots");
+    }
+}
